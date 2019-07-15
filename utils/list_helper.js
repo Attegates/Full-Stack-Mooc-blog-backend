1b1ccfc0c4d6818d3e405fcd9f8ef4b7e6794a8b
Atte Laakso <laakso95@gmail.com>
2019-07-15 14:50:01 +0300

    Exercise 4.4

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -2,6 +2,11 @@ const dummy = (blogs) => {
   return 1
 }
 
+const totalLikes = (blogs) => {
+  return blogs.map(b => b.likes).reduce((prev, next) => prev + next, 0)  
+}
+
 module.exports = {
-  dummy
+  dummy,
+  totalLikes
 }
