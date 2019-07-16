3385f4a2a88fa485792d7d7433db2bd6ccf301d8
Atte Laakso <laakso95@gmail.com>
2019-07-16 08:12:37 +0300

    Exercise 4.5

--- tests/listHelper.test.js
+++ tests/listHelper.test.js
@@ -48,7 +48,7 @@ const blogs = [
     url: "http://blog.cleancoder.com/uncle-bob/2016/05/01/TypeWars.html",
     likes: 2,
     __v: 0
-  }  
+  }
 ]
 
 test('dummy returns one', () => {
@@ -84,4 +84,40 @@ describe('total likes', () => {
     const result = listHelper.totalLikes([])
     expect(result).toBe(0)
   })
+})
+
+describe('favorite blogs', () => {
+
+  test('when list has only one blog returns that blog', () => {
+    const listWithOneBlog = [
+      {
+        _id: '5a422aa71b54a676234d17f8',
+        title: 'Go To Statement Considered Harmful',
+        author: 'Edsger W. Dijkstra',
+        url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
+        likes: 5,
+        __v: 0
+      }
+    ]
+    const result = listHelper.favoriteBlogs(listWithOneBlog)
+    expect(result).toEqual(listWithOneBlog[0])
+  })
+
+  test('when list has multiple blogs return one with most likes', () => {
+    const shouldEqual = {
+      _id: "5a422b3a1b54a676234d17f9",
+      title: "Canonical string reduction",
+      author: "Edsger W. Dijkstra",
+      url: "http://www.cs.utexas.edu/~EWD/transcriptions/EWD08xx/EWD808.html",
+      likes: 12,
+      __v: 0
+    }
+    const result = listHelper.favoriteBlogs(blogs)
+    expect(result).toEqual(shouldEqual)
+  })
+
+  test('when list is empty returns null', () => {
+    expect(listHelper.favoriteBlogs([])).toBe(null)
+  })
+
 })
--- utils/list_helper.js
+++ utils/list_helper.js
@@ -3,10 +3,21 @@ const dummy = (blogs) => {
 }
 
 const totalLikes = (blogs) => {
-  return blogs.map(b => b.likes).reduce((prev, next) => prev + next, 0)  
+  return blogs.map(b => b.likes).reduce((prev, next) => prev + next, 0)
+}
+
+// find the blog with most votes. does not care if multiple blogs have the same amount of likes 
+const favoriteBlogs = (blogs) => {
+  if (blogs.length > 0) {
+    return blogs.reduce((prev, current) => {
+      return (prev.likes > current.likes) ? prev : current
+    })
+  }
+  return null
 }
 
 module.exports = {
   dummy,
-  totalLikes
+  totalLikes,
+  favoriteBlogs
 }
