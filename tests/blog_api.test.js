9be640e94cce82b3284301a46eb4e4a5da00d166
Atte Laakso <laakso95@gmail.com>
2019-07-17 10:48:03 +0300

    Organize tests

--- tests/blog_api.test.js
+++ tests/blog_api.test.js
@@ -8,117 +8,126 @@ const api = supertest(app)
 
 const API_BASE_URL = '/api/blogs'
 
-beforeEach(async () => {
-  await Blog.deleteMany({})
+describe('when there is initial blogs saved', () => {
 
-  const blogObjects = helper.initialBlogs.map(blog => new Blog(blog))
 
-  const promiseArray = blogObjects.map(blog => blog.save())
-  await Promise.all(promiseArray)
 
-})
+  beforeEach(async () => {
+    await Blog.deleteMany({})
 
-test('all blogs are returned', async () => {
-  const response = await api.get(API_BASE_URL)
-  expect(response.body.length).toBe(helper.initialBlogs.length)
-})
+    const blogObjects = helper.initialBlogs.map(blog => new Blog(blog))
 
-test('returned blogs have identifier field defined and it is defined as "id"', async () => {
-  const blogs = await helper.blogsInDb()
+    const promiseArray = blogObjects.map(blog => blog.save())
+    await Promise.all(promiseArray)
 
-  blogs.forEach(blog => {
-    expect(blog.id).toBeDefined()
   })
 
-})
-
-
-test('a valid blog can be added', async () => {
-  const newBlog = {
-    title: "BLACK METAL IST KRIEG",
-    author: "Atte Gates",
-    url: "666",
-    likes: 14,
-  }
-
-  await api
-    .post(API_BASE_URL)
-    .send(newBlog)
-    .expect(200)
-    .expect('Content-Type', /application\/json/)
-
-  const blogsAtEnd = await helper.blogsInDb()
-  expect(blogsAtEnd.length).toBe(helper.initialBlogs.length + 1)
-
-  expect(blogsAtEnd).toContainEqual({
-    id: expect.any(String),
-    title: "BLACK METAL IST KRIEG",
-    author: "Atte Gates",
-    url: "666",
-    likes: 14,
+  test('all blogs are returned', async () => {
+    const response = await api.get(API_BASE_URL)
+    expect(response.body.length).toBe(helper.initialBlogs.length)
   })
 
-})
+  test('returned blogs have identifier field defined and it is defined as "id"', async () => {
+    const blogs = await helper.blogsInDb()
 
-test('likes default to 0 when a blog that does not define likes is added', async () => {
-  const newBlog = {
-    title: "BLACK METAL IST KRIEG",
-    author: "Atte Gates",
-    url: "666",
-  }
+    blogs.forEach(blog => {
+      expect(blog.id).toBeDefined()
+    })
 
-  await api
-    .post(API_BASE_URL)
-    .send(newBlog)
-    .expect(200)
-    .expect('Content-Type', /application\/json/)
-
-  const blogsAtEnd = await helper.blogsInDb()
-  expect(blogsAtEnd).toContainEqual({
-    id: expect.any(String),
-    title: "BLACK METAL IST KRIEG",
-    author: "Atte Gates",
-    url: "666",
-    likes: 0,
   })
 
-})
 
-test('when blog does not define fields "title" or "url" it is not added and response status is 400', async () => {
-  let newBlog = {
-    author: "Atte Gates",
-    likes: 14
-  }
+  describe('adding a blog', () => {
 
-  await api
-    .post(API_BASE_URL)
-    .send(newBlog)
-    .expect(400)
 
-  newBlog = {
-    author: "Atte Gates",
-    likes: 14,
-    title: "BLACK METAL IST KRIEG"
-  }
+    test('a valid blog can be added', async () => {
+      const newBlog = {
+        title: "BLACK METAL IST KRIEG",
+        author: "Atte Gates",
+        url: "666",
+        likes: 14,
+      }
 
-  await api
-    .post(API_BASE_URL)
-    .send(newBlog)
-    .expect(400)
+      await api
+        .post(API_BASE_URL)
+        .send(newBlog)
+        .expect(200)
+        .expect('Content-Type', /application\/json/)
 
-  newBlog = {
-    author: "Atte Gates",
-    likes: 14,
-    url: "666"
-  }
+      const blogsAtEnd = await helper.blogsInDb()
+      expect(blogsAtEnd.length).toBe(helper.initialBlogs.length + 1)
 
-  await api
-    .post(API_BASE_URL)
-    .send(newBlog)
-    .expect(400)
+      expect(blogsAtEnd).toContainEqual({
+        id: expect.any(String),
+        title: "BLACK METAL IST KRIEG",
+        author: "Atte Gates",
+        url: "666",
+        likes: 14,
+      })
 
-  const blogsAtEnd = await helper.blogsInDb()
-  expect(blogsAtEnd.length).toBe(helper.initialBlogs.length)
+    })
+
+    test('likes default to 0 when a blog that does not define likes is added', async () => {
+      const newBlog = {
+        title: "BLACK METAL IST KRIEG",
+        author: "Atte Gates",
+        url: "666",
+      }
+
+      await api
+        .post(API_BASE_URL)
+        .send(newBlog)
+        .expect(200)
+        .expect('Content-Type', /application\/json/)
+
+      const blogsAtEnd = await helper.blogsInDb()
+      expect(blogsAtEnd).toContainEqual({
+        id: expect.any(String),
+        title: "BLACK METAL IST KRIEG",
+        author: "Atte Gates",
+        url: "666",
+        likes: 0,
+      })
+
+    })
+
+    test('when blog does not define fields "title" or "url" it is not added and response status is 400', async () => {
+      let newBlog = {
+        author: "Atte Gates",
+        likes: 14
+      }
+
+      await api
+        .post(API_BASE_URL)
+        .send(newBlog)
+        .expect(400)
+
+      newBlog = {
+        author: "Atte Gates",
+        likes: 14,
+        title: "BLACK METAL IST KRIEG"
+      }
+
+      await api
+        .post(API_BASE_URL)
+        .send(newBlog)
+        .expect(400)
+
+      newBlog = {
+        author: "Atte Gates",
+        likes: 14,
+        url: "666"
+      }
+
+      await api
+        .post(API_BASE_URL)
+        .send(newBlog)
+        .expect(400)
+
+      const blogsAtEnd = await helper.blogsInDb()
+      expect(blogsAtEnd.length).toBe(helper.initialBlogs.length)
+    })
+  })
 })
 
 afterAll(() => {
