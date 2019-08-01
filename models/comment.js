e680b30774d84effe640b49c5574d506a8c78d9d
Atte Laakso <laakso95@gmail.com>
2019-08-01 12:48:54 +0300

    add id to returned Comment JSON

--- models/comment.js
+++ models/comment.js
@@ -11,6 +11,7 @@ const commentSchema = new mongoose.Schema({
 
 commentSchema.set('toJSON', {
   transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString()
     delete returnedObject._id
     delete returnedObject.__v
   }
